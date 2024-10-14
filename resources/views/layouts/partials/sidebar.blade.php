9ab90ff14a1146a7c1690114385dec7f67e3a5f4
Hafiz Caniago <hafizcode02@gmail.com>
2024-10-14 16:29:00 +0700

    feat: update admin profile photo

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="dist/img/avatar.png" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">{{ Auth::user()->name }}</a>
@@ -27,7 +27,7 @@
                  with font-awesome or any other icon font library -->
                     <li class="nav-header">MENU UTAMA</li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="pages/gallery.html" class="nav-link active">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
                                 Dashboard
@@ -51,8 +51,8 @@
                         </a>
                     </li>
                     <li class="nav-header">DOKUMEN SURAT</li>
-                    <li class="nav-item menu-open">
-                        <a href="#" class="nav-link active">
+                    <li class="nav-item menu-close">
+                        <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-book"></i>
                             <p>
                                 Sekretariat
@@ -61,7 +61,7 @@
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="./index.html" class="nav-link active">
+                                <a href="./index.html" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Surat Masuk</p>
                                 </a>
